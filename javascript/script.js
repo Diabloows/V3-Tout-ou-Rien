bf5728469157b28e457558d750af117d3aaed9bd
Matthieu Carrara <matthieu6019@gmail.com>
2021-08-11 15:14:59 +0200

    DECLARATION DES CONSTANTES DU DOM ET AJOUT DES PHOTOS POUR LA VARIABLE DU DES

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,22 @@ $(document).ready(function(){
  });
 });
 
+
+/* Constantes du DOM */
+const buttonDiceThrow = document.getElementById('throw');
+const hold = document.getElementById('hold');
+const newGame = document.getElementById('new-game');
+const text1 = document.getElementById('p-text1');
+const stateText = document.getElementById('p-state');
+const winText = document.getElementById('p-win');
+const diceImage = document.getElementById('dice-image');
+
+/* IMAGES */
+var diceImageToNumber = {
+  1: "Images/Pierre1.png",
+  2: "Images/Pierre2.png" ,
+  3: "Images/Pierre3.png" ,
+  4: "Images/Pierre4.png" ,
+  5: "Images/Pierre5.png" ,
+  6: "Images/Pierre6.png"
+};
